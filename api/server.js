cbe12fd4d1d2e3fa1f56642cc2d090ac268468a1
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-12-10 19:52:16 +0100

    installed dependencies

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
 // build your server here and require it from index.js
+const express = require("express")
+const projectRouter = require('./project/router')
+const server = express()
+
+server.use(express.json())
+
+server.use('api/project', projectRouter)
+
+server.use('*', (req,res,next) => {
+    next({status:404, message:'not found!'})
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+module.exports = server
